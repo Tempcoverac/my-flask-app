ac071c45cad40cfd0c9738e47aef4090dbd94d80
Tempcoverac <noreply@tempcover.ac>
2024-11-06 17:57:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ users = {
 @auth.verify_password
 def verify_password(username, password):
     if username in users and check_password_hash(users.get(username), password):
-        return username
+        return username  
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
