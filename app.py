312cec6a26b6786dc743abd36aad84eef355d49d
Tempcoverac <noreply@tempcover.ac>
2024-11-29 19:58:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def verify_password(username, password):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    policy_number = request.form.get('policy_num')
+    policy_number = None
     if request.method == 'POST':
         # Get data from the form submission
         last_name = request.form.get('surname').lower()
